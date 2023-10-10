7e7cfe0359f2d4e5dd3f4abfcd46a7aa9a589c20
RickCraco <riccardocracolici43@gmail.com>
2023-10-10 15:41:23 +0200

    migliorata la condizione

--- js/script.js
+++ js/script.js
@@ -11,21 +11,32 @@ generateBtn.addEventListener('click',
         km = parseInt(document.getElementById('km').value);
         age = document.getElementById('age').value;
 
-        if(isNaN(km)){
-            alert('inserire valori validi')
-        }
+        //if(isNaN(km)){
+        //    alert('inserire valori validi')
+        //}else if(nome === '' || nome === isNaN(nome)){
+            //alert('inserire valori validi')
+        //}
 
-        const basePrice = km * 0.21;
-
-        if(age === 'minorenne'){
-            result = basePrice - (basePrice * 0.20);
-        }else if(age === 'over65'){
-            result = basePrice - (basePrice * 0.40);
+        if(nome === '' || !isNaN(parseInt(nome))){
+            alert('inserire valori validi');
+            
+        }else if (isNaN(km)){
+            alert('inserire valori validi');
         }else{
-            result = basePrice;
+            const basePrice = km * 0.21;
+
+            if(age === 'minorenne'){
+                result = basePrice - (basePrice * 0.20);
+            }else if(age === 'over65'){
+                result = basePrice - (basePrice * 0.40);
+            }else{
+                result = basePrice;
+            }
+
+            console.log(result);
         }
 
-        console.log(result);
+        
     }
 
 )
