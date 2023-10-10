db7363f8c0c7b085acfeb27410f7efe8f4377d06
RickCraco <riccardocracolici43@gmail.com>
2023-10-10 14:57:04 +0200

    aggiunta funziona onclick che calcola il prezzo

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
 const generateBtn = document.getElementById('generate');
+let nome;
+let km;
+let age;
+let result;
+
 generateBtn.addEventListener('click', 
 
     function(){
-        
+        nome = document.getElementById('nome').value;
+        km = parseInt(document.getElementById('km').value);
+        age = document.getElementById('age').value;
+
+        if(isNaN(km)){
+            alert('inserire valori validi')
+        }
+
+        const basePrice = km * 0.21;
+
+        if(age === 'minorenne'){
+            result = basePrice - (basePrice * 0.20);
+        }else if(age === 'over65'){
+            result = basePrice - (basePrice * 0.40);
+        }else{
+            result = basePrice;
+        }
+
+        console.log(result);
     }
 
 )
