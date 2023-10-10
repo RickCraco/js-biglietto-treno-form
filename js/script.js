41ed9e1c4cd71ff4131969933433df0b52731054
RickCraco <riccardocracolici43@gmail.com>
2023-10-10 17:10:20 +0200

    aggiunto visibilitá biglietto nell'html alla generazione del biglietto

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let nome;
 let km;
 let age;
 let result;
+const ticket = document.getElementById('ticket');
 
 generateBtn.addEventListener('click', 
 
@@ -28,16 +29,36 @@ generateBtn.addEventListener('click',
 
             if(age === 'minorenne'){
                 result = basePrice - (basePrice * 0.20);
+                document.querySelector('.offerta').innerHTML = 'Offerta minorenni';
             }else if(age === 'over65'){
                 result = basePrice - (basePrice * 0.40);
+                document.querySelector('.offerta').innerHTML = 'Offerta over 65';
             }else{
                 result = basePrice;
+                document.querySelector('.offerta').innerHTML = 'Offerta standard';
             }
+
+            ticket.classList.remove('d-none');  
+            document.querySelector('.nome-pass').innerHTML = nome;
+            document.querySelector('.carrozza').innerHTML = Math.floor(Math.random() * 11);
+            document.querySelector('.cp').innerHTML = Math.floor(Math.random() * 10000);
+            document.querySelector('.costo').innerHTML = result;
 
             console.log(result);
         }
 
         
+    }
+
+)
+
+resetBtn.addEventListener('click',
+
+    function(){
+        document.getElementById('nome').value = '';
+        document.getElementById('km').value = '';
+        document.getElementById('age').value = 'minorenne';
+        ticket.classList.add('d-none')
     }
 
 )
